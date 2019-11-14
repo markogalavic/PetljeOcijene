e241b0144201911d239f84bfdd07b5c5d87455fd
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-11-14 11:26:35 +0100

    Add files via upload
    Unos ocijena i prosjek

---
+++ galavićMarkoPetljeOcijene/galavićMarkoPetljeOcijene/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoPetljeOcijene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocijena;
+            float prosjek;
+            float brojOcijena = 0;
+            int zbrojOcijena = 0;
+               do
+          {
+                Console.WriteLine("Upišite ocijenu");
+                ocijena = Convert.ToInt16(Console.ReadLine());
+
+
+                if (ocijena == 1)
+                {
+                    prosjek = 1;
+                    break;
+                }
+                else if (ocijena > 5 || ocijena < 0)
+                {
+                    Console.WriteLine("Pogrešan unos. Unesite ponovo.");
+                }
+                else if(ocijena != 0)
+                {
+                    brojOcijena++;
+                    zbrojOcijena += ocijena;
+                }
+
+                prosjek = zbrojOcijena / brojOcijena;
+            }
+                while (ocijena != 0) ;
+            Console.WriteLine("Prosjek ocjena je " + prosjek);
+
+          
+
+
+
+            Console.ReadKey();
+
+         }
+    }
+}
